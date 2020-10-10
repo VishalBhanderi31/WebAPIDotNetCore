5bd782de18b9952202f6b3542610ca8caca8db99
VisahlBhanderi31 <VisahlBhanderi07@gmail.com>
2020-10-10 23:30:21 +0530

    Add virtual in BookCategory Model

--- WebAPICore/Models/Category.cs
+++ WebAPICore/Models/Category.cs
@@ -9,6 +9,6 @@ namespace WebAPICore.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public ICollection<BookCategory> BookCategories { get; set; }
+        public virtual ICollection<BookCategory> BookCategories { get; set; }
     }
 }
