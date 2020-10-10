6d837e0bffd3469663a55ac75afa0cbe76616d6a
VisahlBhanderi31 <VisahlBhanderi07@gmail.com>
2020-10-10 23:22:40 +0530

    Add Relation between all various Models

--- WebAPICore/Models/Author.cs
+++ WebAPICore/Models/Author.cs
@@ -10,6 +10,9 @@ namespace WebAPICore.Models
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public virtual Country Country { get; set; }
+        public virtual ICollection<BookAuthor> BookAuthors { get; set; }
+
 
     }
 }
